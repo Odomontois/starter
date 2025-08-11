76294cc15390b423798ed7f05284147dbac1a5c4
Oleg Nizhnikov <odomontois@gmail.com>
2025-08-11 11:29:27 +0200

    Added Metals and LazyJJ

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+local map = LazyVim.safe_keymap_set
+local wk = require("which-key")
+
+wk.add({ "<leader>m", group = "Metals", icon = { icon = "", color = "red" } })
+wk.add({ "<;leader>j", group = "Jujutsu" })
+
+local function toggleSetting(modname, setting)
+  return function()
+    require(modname).toggle_setting(setting)
+  end
+end
+
+map("n", "<leader>ma", toggleSetting("metals", "showImplicitArguments"), { desc = "Show Implicit Arguments" })
+
+map("n", "<leader>mi", toggleSetting("metals", "showImplicitConversionsAndClasses"), { desc = "Show Implicit Conversions" })
+
+map("n", "<leader>mf", "<cmd>MetalsNewScalaFile<cr>", { desc = "Create scala file" })
+
+map("n", "<leader>ml", toggleSetting("metals", "allowCaptureChecking"), { desc = "Allow Capture Checking" })
---
+++ lua/plugins/scala.lua
@@ -0,0 +1,113 @@
+return {
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", "java" },
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.settings = {
+        serverVersion = "1.6.2",
+      }
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "scala" } },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      -- Debug settings
+      local dap = require("dap")
+      dap.configurations.scala = {
+        {
+          type = "scala",
+          request = "launch",
+          name = "RunOrTest",
+          metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+          },
+        },
+        {
+          type = "scala",
+          request = "launch",
+          name = "Test Target",
+          metals = {
+            runType = "testTarget",
+          },
+        },
+      }
+    end,
+  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       metals = {
+  --         keys = {
+  --           {
+  --             "<leader>me",
+  --             function()
+  --               require("telescope").extensions.metals.commands()
+  --             end,
+  --             desc = "Metals commands",
+  --           },
+  --           {
+  --             "<leader>mc",
+  --             function()
+  --               require("metals").compile_cascade()
+  --             end,
+  --             desc = "Metals compile cascade",
+  --           },
+  --           {
+  --             "<leader>mh",
+  --             function()
+  --               require("metals").hover_worksheet()
+  --             end,
+  --             desc = "Metals hover worksheet",
+  --           },
+  --         },
+  --         init_options = {
+  --           statusBarProvider = "off",
+  --         },
+  --         settings = {
+  --           showImplicitArguments = true,
+  --           excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+  --         },
+  --       },
+  --     },
+  --     setup = {
+  --       metals = function(_, opts)
+  --         local metals = require("metals")
+  --         local metals_config = vim.tbl_deep_extend("force", metals.bare_config(), opts)
+  --         metals_config.on_attach = LazyVim.has("nvim-dap") and metals.setup_dap or nil
+  --
+  --         local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+  --         vim.api.nvim_create_autocmd("FileType", {
+  --           pattern = { "scala", "sbt" },
+  --           callback = function()
+  --             metals.initialize_or_attach(metals_config)
+  --           end,
+  --           group = nvim_metals_group,
+  --         })
+  --         return true
+  --       end,
+  --     },
+  --   },
+  -- },
+}
